90bf2f60ecc539e4fb0bc55a623d61bc9651c893
hamrosh <hamroshroy@gmail.com>
2018-04-27 20:13:22 +0530

    first commit

---
+++ server/Schema/Question.js
@@ -0,0 +1,12 @@
+const Question = `
+type Question{
+    question:String
+    opt1:String
+    opt2:String
+    opt3:String
+    opt4:String
+    rightopt:String
+}
+`;
+
+module.exports = Question;
